f869894139b1fff1b3ae418c410f4bc79f0057a4
vishaldeepverma <vishaldeepverma@live.in>
2021-01-09 05:07:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,321 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Data from "./data";
+
+export default function App() {
+  // initialise products
+  const [products, setProducts] = useState(Data);
+
+  const [filteredProducts, setFilteredProducts] = useState(Data);
+
+  // set index
+  const [index, setIndex] = useState(0);
+
+  // selected category
+
+  const [category, setCategory] = useState(0);
+
+  // update category and filter data
+  const updateCategory = (id) => {
+    setIndex(0);
+    setCategory(id);
+    const data =
+      id === 0
+        ? Data
+        : products.filter((product) => product.categoryCode === id);
+    console.log(data);
+    setFilteredProducts(data);
+  };
+
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          display: "flex",
+          "align-items": "center",
+          marginBottom: "50px",
+          justifyContent: "center",
+          fontSize: "25px"
+        }}
+      >
+        <div
+          style={{
+            marginRight: "20px",
+            width: "100px",
+
+            padding: "10px",
+            alignItems: "center",
+            cursor: "pointer",
+            textDecoration: category === 0 ? "underline" : ""
+          }}
+          onClick={() => updateCategory(0)}
+        >
+          All
+        </div>
+
+        <div
+          style={{
+            marginRight: "20px",
+            width: "100px",
+
+            padding: "10px",
+            alignItems: "center",
+            cursor: "pointer",
+            textDecoration: category === 1 ? "underline" : ""
+          }}
+          onClick={() => updateCategory(1)}
+        >
+          Shoes
+        </div>
+        <div
+          style={{
+            marginRight: "20px",
+            width: "100px",
+
+            padding: "10px",
+            alignItems: "center",
+            cursor: "pointer",
+            textDecoration: category === 2 ? "underline" : ""
+          }}
+          onClick={() => updateCategory(2)}
+        >
+          Electronics
+        </div>
+        <div
+          style={{
+            marginRight: "20px",
+            width: "100px",
+
+            padding: "10px",
+            alignItems: "center",
+            cursor: "pointer",
+            textDecoration: category === 3 ? "underline" : ""
+          }}
+          onClick={() => updateCategory(3)}
+        >
+          Clothing
+        </div>
+      </div>
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          "align-items": "center",
+          justifyContent: "center"
+        }}
+      >
+        <p
+          onClick={() => (index ? setIndex(index - 1) : "")}
+          style={{
+            "background-color": index ? "black" : "grey",
+            cursor: "pointer",
+            "border-radius": "10px",
+            color: "white",
+            margin: "5px",
+            padding: "10px",
+            display: "inline-block"
+          }}
+        >
+          {"<"}
+        </p>
+
+        {/* show all three products */}
+        <React.Fragment>
+          <div
+            style={{
+              height: "380px",
+              maxHeight: "380px",
+              backgroundColor: "white",
+              // border: "1px solid",
+              margin: "5px",
+              width: "300px",
+              maxWidth: "300px",
+              boxShadow: "3px 3px 5px 6px #ccc"
+            }}
+          >
+            <div
+              style={{
+                backgroundImage: `url(${
+                  filteredProducts[index] ? filteredProducts[index].image : ""
+                })`,
+                backgroundPosition: "center",
+                backgroundSize: "contain",
+                marginTop: "20px",
+                height: "215px",
+                maxHeight: "215px",
+                width: "200px",
+
+                backgroundRepeat: "no-repeat",
+                display: "inline-block"
+              }}
+            ></div>
+            <div
+              style={{
+                fontWeight: "bold",
+                fontSize: "15px",
+                marginLeft: "10px",
+                marginRight: "10px"
+              }}
+            >
+              <div style={{ marginTop: "5%" }}>
+                Name: {filteredProducts[index].itemName}
+              </div>
+              <div style={{ marginTop: "5px" }}>
+                Price: <span>₹{filteredProducts[index].itemPrice}.00</span>
+                <span style={{ textDecoration: "line-through", color: "grey" }}>
+                  {" ₹"}
+                  {filteredProducts[index].itemMrp}.00
+                </span>
+              </div>
+              <div style={{ marginTop: "5px" }}>
+                Category: {filteredProducts[index].category}
+              </div>
+            </div>
+          </div>
+
+          {filteredProducts[index + 1] ? (
+            <div
+              style={{
+                height: "420px",
+                maxHeight: "420px",
+                backgroundColor: "white",
+                // border: "1px solid",
+                margin: "5px",
+                width: "360px",
+                maxWidth: "360px",
+                boxShadow: "3px 3px 5px 6px #ccc"
+              }}
+            >
+              <div
+                style={{
+                  backgroundImage: `url(${
+                    filteredProducts[index + 1]
+                      ? filteredProducts[index + 1].image
+                      : ""
+                  })`,
+                  backgroundPosition: "center",
+                  backgroundSize: "contain",
+                  marginTop: "20px",
+
+                  backgroundRepeat: "no-repeat",
+                  height: "270px",
+                  maxWidth: "250px",
+                  minWidth: "250px",
+                  display: "inline-block"
+                }}
+              ></div>
+
+              <div
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                  marginLeft: "10px",
+                  marginRight: "10px"
+                }}
+              >
+                <div style={{ marginTop: "5%" }}>
+                  Name:{filteredProducts[index + 1].itemName}
+                </div>
+                <div style={{ marginTop: "5px" }}>
+                  Price:{" "}
+                  <span>₹{filteredProducts[index + 1].itemPrice}.00</span>
+                  <span
+                    style={{ textDecoration: "line-through", color: "grey" }}
+                  >
+                    {" ₹"}
+                    {filteredProducts[index].itemMrp}.00
+                  </span>
+                </div>
+                <div style={{ marginTop: "5px" }}>
+                  Category:{filteredProducts[index + 1].category}
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+
+          {filteredProducts[index + 2] ? (
+            <div
+              style={{
+                height: "380px",
+                maxHeight: "380px",
+                backgroundColor: "white",
+                // border: "1px solid",
+                margin: "5px",
+                width: "300px",
+                maxWidth: "300px",
+                boxShadow: "3px 3px 5px 6px #ccc"
+              }}
+            >
+              <div
+                style={{
+                  backgroundImage: `url(${
+                    filteredProducts[index + 2]
+                      ? filteredProducts[index + 2].image
+                      : ""
+                  })`,
+                  backgroundPosition: "center",
+                  backgroundSize: "contain",
+                  marginTop: "20px",
+
+                  backgroundRepeat: "no-repeat",
+                  height: "215px",
+                  maxHeight: "215px",
+                  width: "200px",
+                  display: "inline-block"
+                }}
+              ></div>
+
+              <div
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                  marginLeft: "10px",
+                  marginRight: "10px"
+                }}
+              >
+                <div style={{ marginTop: "5%" }}>
+                  Name:{filteredProducts[index + 2].itemName}
+                </div>
+                <div style={{ marginTop: "5px" }}>
+                  Price:{" "}
+                  <span>₹{filteredProducts[index + 2].itemPrice}.00</span>
+                  <span
+                    style={{ textDecoration: "line-through", color: "grey" }}
+                  >
+                    {" ₹"}
+                    {filteredProducts[index].itemMrp}.00
+                  </span>
+                </div>
+                <div style={{ marginTop: "5px" }}>
+                  Category:{filteredProducts[index + 2].category}
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </React.Fragment>
+
+        <p
+          onClick={() =>
+            index === filteredProducts.length - 3 ? "" : setIndex(index + 1)
+          }
+          style={{
+            "background-color":
+              index !== filteredProducts.length - 3 ? "black" : "grey",
+            cursor: index !== filteredProducts.length - 3 ? "pointer" : "",
+            "border-radius": "10px",
+            color: "white",
+            margin: "5px",
+            padding: "10px",
+            display: "inline-block"
+          }}
+        >
+          {">"}
+        </p>
+      </div>
+    </React.Fragment>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,192 @@
+const data = [
+  // shoes 1
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/jmwch3k0/shoe/j/y/n/dg-292-white-blue-patti-10-digitrendzz-white-original-imaf9p36fkykfjqt.jpeg",
+    itemName: "White Sneaker For Men's/Boy's Sneakers For Men  (White)",
+    itemPrice: "667",
+    itemMrp: "999",
+    category: "Shoes",
+    categoryCode: 1
+  },
+  // shoes 2
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/k6jnfrk0/shoe/z/x/9/171-8-pexlo-black-tan-original-imafzqbthfzkqv76.jpeg?q=50",
+    itemName: "171 Smart Tan Lace-Ups Casuals for Men Sneakers For Men  (Tan)",
+    itemPrice: "876",
+    itemMrp: "999",
+    category: "Shoes",
+    categoryCode: 1
+  },
+  // shoes 3
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/k3hmj680/shoe/q/m/s/walking-shoes-9-absolute-comfort-black-white-original-imafmm23ubahj36q.jpeg?q=50",
+    itemName: "Walking shoes Running Shoes For Men  (Black, White)",
+    itemPrice: "456",
+    itemMrp: "999",
+    category: "Shoes",
+    categoryCode: 1
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/jw0zr0w0/shoe/m/v/x/combo-2-1214-606-6-axter-multicolor-original-imaey4zgqeek9syc.jpeg?q=50",
+    itemName: "Hiking & Trekking Shoes For Men  (Multicolor)",
+    itemPrice: "456",
+    itemMrp: "999",
+    category: "Shoes",
+    categoryCode: 1
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/keekmfk0-0/shoe/b/p/k/bouncer-01cgryorng-6-asian-grey-original-imafv3h4hzjgvw2z.jpeg?q=50",
+    itemName: "Bouncer-01 Running shoes for boys",
+    itemPrice: "456",
+    itemMrp: "999",
+    category: "Shoes",
+    categoryCode: 1
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/k40798w0/shoe/f/z/y/socks-04-grey-11-density-grey-original-imafgv9z35he5bfv.jpeg?q=50",
+    itemName: "SOCKS Running Shoes For Men  (Grey)",
+    itemPrice: "456",
+    itemMrp: "999",
+    category: "Shoes",
+    categoryCode: 1
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/k3lwuq80/shoe/z/v/h/mrj1324-6-aadi-black-original-imafmzfcjcjytgcf.jpeg?q=50",
+    itemName: "Running Shoes For Men  (Black)",
+    itemPrice: "456",
+    itemMrp: "999",
+    category: "Shoes",
+    categoryCode: 1
+  },
+  // product 1
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/k12go7k0/trimmer/e/z/r/xxq01hm-mi-original-imafknhtkmezde6p.jpeg?q=70",
+    itemName: "White Sneaker For Men's/Boy's Sneakers For Men  (White)",
+    itemPrice: "667",
+    itemMrp: "999",
+    category: "Electronics",
+    categoryCode: 2
+  },
+
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/k0igia80/smart-lighting/f/t/r/sb0110-b22-led-smitch-original-imafka9umvxwrecq.jpeg?q=70",
+    itemName: "Smitch Wi-Fi White Ambience (6500k) - (10W) B22 Base Smart Bulb",
+    itemPrice: "667",
+    itemMrp: "999",
+    category: "Electronics",
+    categoryCode: 2
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/jsw3yq80/speaker/soundbar/a/z/m/philips-mms8085b-94-original-imaex42ejqg9ytfj.jpeg?q=70",
+    itemName:
+      "Philips MMS8085B/94 Convertible 80 W Bluetooth Home Theatre  (Black, 2.1 Channel)",
+    itemPrice: "667",
+    itemMrp: "999",
+    category: "Electronics",
+    categoryCode: 2
+  },
+
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/k5fn3ww0/computer/c/n/f/hp-na-original-imafz4duhht4zbnx.jpeg?q=70",
+    itemName: "HP 15 Ryzen 3 Dual Core 3200U",
+    itemPrice: "667",
+    itemMrp: "999",
+    category: "Electronics",
+    categoryCode: 2
+  },
+  // product 2
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/416/416/khdqnbk0/pulse-oximeter/h/b/s/dr-trust-usa-model-210-fingertip-oxy-meter-finger-oxygen-original-imafxekyqwzqjde5.jpeg?q=70",
+    itemName: "171 Smart Tan Lace-Ups Casuals for Men Sneakers For Men  (Tan)",
+    itemPrice: "876",
+    itemMrp: "999",
+    category: "Electronics",
+    categoryCode: 2
+  },
+
+  // skirts 1
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/kit6hzk0-0/kids-dress/4/r/a/9-10-years-checks-rebjacket-ftc-fashions-original-imafygsguzgyrekg.jpeg?q=50",
+    itemName: "White Sneaker For Men's/Boy's Sneakers For Men  (White)",
+    itemPrice: "667",
+    itemMrp: "999",
+    category: "Clothing",
+    categoryCode: 3
+  },
+  // skirts 2
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/k6mibgw0/kids-dress/d/5/4/10-11-years-blueribjacket-ftc-fashions-original-imafz5g6mffxnkd9.jpeg?q=50",
+    itemName: "171 Smart Tan Lace-Ups Casuals for Men Sneakers For Men  (Tan)",
+    itemPrice: "876",
+    itemMrp: "999",
+    category: "Clothing",
+    categoryCode: 3
+  },
+  // skirts 3
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/k5r2mq80/kids-dress/g/g/h/10-11-years-multi-ftc-fashions-original-imafzde86edxjfrp.jpeg?q=50",
+    itemPrice: "456",
+    itemName: "171 Smart Tan Lace-Ups Casuals for Men Sneakers For Men  (Tan)",
+
+    itemMrp: "999",
+    category: "Clothing",
+    categoryCode: 3
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/kelptow0-0/trouser/m/g/e/xxl-kx-crpe-age-mlt-s-kex-original-imafv8yw3g9afyug.jpeg?q=50",
+    itemPrice: "456",
+    itemName: "Regular Fit Women Maroon Crepe Trousers",
+
+    itemMrp: "999",
+    category: "Clothing",
+    categoryCode: 3
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/jhdq9ow0/trouser/n/c/d/28-emz00069-emeros-original-imaf5erw74ysqggc.jpeg?q=50",
+    itemPrice: "456",
+    itemName: "Flared Women Multicolor, White Poly Crepe Trousers",
+
+    itemMrp: "999",
+    category: "Clothing",
+    categoryCode: 3
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/k5o7r0w0/trouser/r/c/t/free-plazo-blue-kalaprayag-original-imafgpgkn5xuuqzh.jpeg?q=50",
+    itemPrice: "456",
+    itemName: "Flared Women Dark Blue Crepe Chiffon Trousers",
+
+    itemMrp: "999",
+    category: "Clothing",
+    categoryCode: 3
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/880/1056/kg8avm80-0/trouser/l/u/1/m-vr1061-varshika-original-imafwgragrwrdsx7.jpeg?q=50",
+    itemPrice: "456",
+    itemName: "Regular Fit Women Blue Rayon Trousers",
+
+    itemMrp: "999",
+    category: "Clothing",
+    categoryCode: 3
+  }
+];
+
+export default data;
